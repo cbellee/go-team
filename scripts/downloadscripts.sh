93c4e5536e2974788d5867eb947cd09252657a4e
Chris Bellee <cbellee@microsoft.com>
2019-12-08 10:14:12 +1100

    remove helm chart

---
+++ scripts/downloadscripts.sh
@@ -0,0 +1,6 @@
+# Download build.sh
+curl https://raw.githubusercontent.com/Microsoft/bedrock/master/gitops/azure-devops/build.sh > build.sh
+chmod +x ./build.sh
+
+curl https://raw.githubusercontent.com/Microsoft/bedrock/master/gitops/azure-devops/release.sh > release.sh
+chmod +x ./release.sh
